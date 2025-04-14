662532172f8a10e29e7ccbcb1fb5d5e9c0930c1d
shar1123 <shchelkunova.arina2314@mail.ru>
2025-04-14 09:29:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -16,36 +16,44 @@ int countPairs2(int *arr, int len, int value) {
   for (int i = 0; i < r; i++) {
     for (int j = r-1; j > l; j++) {
       if (arr[j] > value) {
-        r -= 1;
+        r--;
       }
       if (arr[i] + arr[j] == value) {
-        k += 1;
+        k++;
       }
     }
-    l += 1;
+    l++;
   }
   return k;
 }
-bool binary(int* arr, int len, int target, int start) {
-  int l = start + 1;
-  int r = len - 1;
+bool binary(int* arr, int l, int r, int target, bool fl) {
+  int result = -1;
   while (l <= r) {
-    int mid = l / 2 + r / 2;
-    if (arr[mid] == target)
-      return true;
-    else if (arr[mid] > target)
-      r = mid - 1;
-    else
+    int mid = l/2 + r/ 2;
+    if (arr[mid] == target) {
+      result = mid;
+      if (fl) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    } else if (arr[mid] < target) {
       l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
   }
-  return false;
+  return result;
 }
-int countPairs3(int *arr, int len, int value) {
+int countPairs3(int* arr, int len, int value) {
   int k = 0;
-  for (int i = 0; i < len-1; i++) {
+  for (int i = 0; i < len; ++i) {
     int target = value - arr[i];
-    if (binary(arr, len, target, i))
-      k++;
+    int first = binary(arr, i + 1, len - 1, target, true);
+    if (first != -1) {
+      int last = binary(arr, first, len - 1, target, false);
+      k += last - first + 1;
+    }
   }
   return k;
 }
